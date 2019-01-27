f92ef839379633778d3a0c5e9dc4698e36f6ee22
anupritaisno1 <www.anuprita804@gmail.com>
2019-01-27 06:55:14 +0000

    build: Ensure /vendor unmounted at install start
    Account for the possibility that a user has mounted vendor manually
    before installing the FullOTA package.
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -483,6 +483,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: %s" % target_fp)
 
   script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
+  script.AppendExtra("ifelse(is_mounted(\"/vendor\"), unmount(\"/vendor\"));")
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
