de97991c42a441fd1d12a2bfa21b303ca31d6ef1
anupritaisno1 <www.anuprita804@gmail.com>
2020-02-03 09:01:39 +0000

    build: make dexpreopt more aggressive
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -56,13 +56,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.dalvik.vm.native.bridge=0
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    pm.dexopt.first-boot=speed \
-    pm.dexopt.boot=speed \
-    pm.dexopt.install=speed \
-    pm.dexopt.bg-dexopt=speed \
-    pm.dexopt.ab-ota=speed \
+    pm.dexopt.first-boot=everything \
+    pm.dexopt.boot=everything \
+    pm.dexopt.install=everything \
+    pm.dexopt.bg-dexopt=everything \
+    pm.dexopt.ab-ota=everything \
     pm.dexopt.inactive=verify \
-    pm.dexopt.shared=speed
+    pm.dexopt.shared=everything
 
 # Enable resolution of startup const strings.
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
